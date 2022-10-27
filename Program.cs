cc2eb9bff414dfe45ae621277f5249258c870b5c
Tiago Furtado <tiagofurtadopfcarneiro@gmail.com>
2022-10-26 21:47:47 -0300

    Subindo projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "41234234", modelo: "Xperia",imei: "41342j3gh423", memoria: 8);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "02938745-", modelo: "Iphone 14 Pro Max",imei: "jd129073rfd092", memoria: 12);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
